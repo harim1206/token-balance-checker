3f9f74048551d89cf92edd7e2f6ef728759462b9
Harim Choi <harim1206@gmail.com>
2022-10-26 11:17:46 -0400

    env test

--- src/library/ethplorer.ts
+++ src/library/ethplorer.ts
@@ -1,5 +1,6 @@
 async function fetchTopTokens () {
   const endpoint = `https://api.ethplorer.io/getTop?apiKey=${process.env.NEXT_PUBLIC_ETHPLORER_API_KEY}&criteria=cap`;
+  console.log('endpoint: ', endpoint);
   const response = await fetch(endpoint);
   const data = await response.json();
   return data;
